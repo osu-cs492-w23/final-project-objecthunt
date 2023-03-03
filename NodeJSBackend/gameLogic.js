162de541e8c48c49a6600e8d4ec7dfd69f9acd0d
Frankreed <baoanhtdn@gmail.com>
2023-03-03 11:55:29 -0800

    the server can now use google vision to check if a submitted image is legit

--- NodeJSBackend/gameLogic.js
+++ NodeJSBackend/gameLogic.js
@@ -1,12 +1,24 @@
 
-// Imports the Google Cloud client library
-const vision = require('@google-cloud/vision');
+async function quickstart(buffer) {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-// Performs label detection on the image file
-const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
-const labels = result.labelAnnotations;
-console.log('Labels:');
-labels.forEach(label => console.log(label.description));
+    const request = {
+        image: {
+            content: buffer.toString("base64"),
+        },
+    };
+
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection(request);
+    const labels = result.labelAnnotations;
+    labels.forEach(label => console.log(label.description))
+    return labels.map(label => label.description.toLowerCase());
+}
+
+module.exports = {
+    "quickstart": quickstart
+}
