6aaa9269645a51680206af97952e465b66a0c9d5
ImgyeongLee <leeim@oregonstate.edu>
2023-03-16 15:32:23 -0700

    delete google lens directory to make it clear

--- AndroidFrontend/app/src/main/java/com/example/activities/CreateActivity.kt
+++ AndroidFrontend/app/src/main/java/com/example/activities/CreateActivity.kt
@@ -3,11 +3,13 @@ package com.example.chatting.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatting.R
 import com.example.chatting.SocketHandler
+import io.socket.client.Ack
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +29,17 @@ class CreateActivity : AppCompatActivity() {
 
         joinBtn.setOnClickListener {
             val nickname = usernameEntry.text.toString()
+            val timelimits = 500
 
             // Check if both values are empty or not
             if (!TextUtils.isEmpty(nickname)) {
-                //mSocket.emit("createRoom")
-                startActivity(intentChat)
+                mSocket.emit("echoTest", "CONNECTED")
+                mSocket.emit("createRoom", nickname, timelimits, Ack { args ->
+                    Log.d("CreateActivity", "Ack $args")
+                    startActivity(intentChat)
+                })
+                Log.d("Create", "Hello?")
+
                 // Need to block the back button or
                 // warn the user that if they press the back button, it will go to the main
             }
