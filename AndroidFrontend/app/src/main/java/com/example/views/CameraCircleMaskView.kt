9436cc70fa59a22f63ad643964475e9f0c331e9a
Adil <bazarkua@oregonstate.edu>
2023-03-21 05:34:12 -0700

    The camera implementation is nearly finished. It now retrieves the current item from the game activity and compares it to the item from the Android ML analyzer. If they match, it dynamically displays that the item has been found, allowing the user to press the 'Take Picture' button. I have also improved the appearance of the camera screen, making it look more attractive with a circular frame. This makes it easier to target the object, similar to a scope.

---
+++ AndroidFrontend/app/src/main/java/com/example/views/CameraCircleMaskView.kt
@@ -0,0 +1,36 @@
+package com.example.views
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class CameraCircleMaskView : View {
+
+    private val paint = Paint().apply {
+        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        isAntiAlias = true
+    }
+
+    private lateinit var rectF: RectF
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rectF = RectF(0f, 0f, w.toFloat(), h.toFloat())
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.WHITE)
+        val cx = rectF.centerX()
+        val cy = rectF.centerY()
+        val radius = Math.min(rectF.width(), rectF.height()) / 2
+        canvas.drawCircle(cx, cy, radius.toFloat(), paint)
+    }
+}
